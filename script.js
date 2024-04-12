eb1d4f4539d392824f673e677eaa15114a8efbcf
Chandrayan Roy <chandrayanroy@gmail.com>
2024-04-12 22:07:28 +0530

    add winning message

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let boxes = document.body.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
+let msgDiv = document.querySelector(".msgDiv");
+let msg = document.querySelector("#winningMsg");
 
 let turnO = true;
 
@@ -19,8 +21,10 @@ boxes.forEach((box) => {
     if (box.innerText === "") {
       if (turnO) {
         box.innerText = "O";
+        msg.innerText = "X's turn";
       } else {
         box.innerText = "X";
+        msg.innerText = "O's turn";
       }
       turnO = !turnO;
       checkWinner();
@@ -37,19 +41,28 @@ const checkWinner = () => {
 
     if (val1 !== "" && val1 === val2 && val2 === val3) {
       displayWinner(val1);
-      return;
     }
   }
 };
 
+const disableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+};
+
 const displayWinner = (winner) => {
-  console.log("Winner is: " + winner);
-  alert(`Winner is: ${winner}`);
+  msg.innerText = `${winner} wins!`;
+  // msgDiv.classList.remove("hideMsg");
+  disableBoxes();
 };
 
 resetBtn.addEventListener("click", () => {
   boxes.forEach((box) => {
     box.innerText = "";
     box.disabled = false;
+    turnO = true;
+    msg.innerText = "O's turn";
+    // msgDiv.classList.add("hideMsg");
   });
 });
